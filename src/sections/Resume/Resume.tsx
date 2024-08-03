a200d26738d122efe3aa062caa35edd25a19f4c8
Han Xheng Chew <81433191+shaaaaame@users.noreply.github.com>
2024-08-03 02:53:44 -0400

    Update Resume.tsx

--- src/sections/Resume/Resume.tsx
+++ src/sections/Resume/Resume.tsx
@@ -65,7 +65,7 @@ export default function Resume() {
             <Grid.Col span={5} h={"70vh"}>
                 <iframe
                     title="resume"
-                    src="https://drive.google.com/file/d/1iE0f6gqUczBxsaFXjuR8p9l7OEwtAN7y/preview"
+                    src="https://drive.google.com/file/d/1SHlucV-bQiPmbbNnPdTgpcee5ctH-5e4/preview"
                     width={"100%"}
                     height={"100%"}
                     style={{ border: "1px solid var(--mantine-color-white)" }}
